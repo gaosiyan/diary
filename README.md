cbbea0326f7c5fb7090e4ea4dadcb3ad50a7cec5
gaozhan <18602912875@163.com>
2025-12-10 20:10:11 +0800

    新建 sphnix 项目

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# diary
-diary
+# 项目说明
+
+## 创建步骤
+
+* 在 github 上创建 diary 仓库:
+    * 添加 README.md;
+    * 按照 Python 模板配置 `.gitignore`  文件.
+* 本地执行 `git clone git@github.com:[username]/diary.git` 下载仓库;
+* 切换到本地仓库目录,依次执行如下命pixi
+
+```bash
+pixi init
+```
+
+
+
+
+
+[diary](https://github.com/gaosiyan/diary)
---
+++ start-vscode.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:: 当前目录压栈并切换到当前脚本所在的目录
+pushd % ~dp0
+
+:: 定义路径
+set pixi_home=D:\bin\pixi-x86_64-pc-windows-msvc
+set python_home=%cd%\.pixi\envs\default
+set vscode_home=D:\bin\vs_code\VSCode-win32-x64-1.101.2_Sphinx
+
+set path=%pixi_home%;%python_home%;%python_home%\Library\mingw-w64\bin;%python_home%\Library\usr\bin;%python_home%\Library\bin;%python_home%\Scripts;%python_home%\bin;%vscode_home%;%path%
+
+start code .
+
+:: 恢复目录
+popd
+
+exit /b 0
