366d43143b6c45a39c2787dd206c218ba2b67816
gaozhan <18602912875@163.com>
2026-01-01 11:38:45 +0800

    123

---
+++ setup-env.bat
@@ -0,0 +1,4 @@
+:: ¶¨ÒåÂ·¾¶
+set pixi_home=D:\bin\pixi-x86_64-pc-windows-msvc
+set python_home=%cd%\.pixi\envs\default
+set vscode_home=D:\bin\vs_code\VSCode-win32-x64-1.85.2_Sphinx
--- start.bat
+++ start.bat
@@ -2,13 +2,7 @@
 
 :: 当前目录压栈并切换到当前脚本所在的目录
 pushd % ~dp0
-
-:: 定义路径
-set pixi_home=D:\bin\pixi-x86_64-pc-windows-msvc
-set python_home=%cd%\.pixi\envs\default
-set vscode_home=D:\bin\vs_code\VSCode-win32-x64-1.107.1_Sphinx
-
+call setup-env.bat
 set path=%pixi_home%;%python_home%;%python_home%\Library\mingw-w64\bin;%python_home%\Library\usr\bin;%python_home%\Library\bin;%python_home%\Scripts;%python_home%\bin;%vscode_home%;%path%
-
 
 sphinx-autobuild source build/html --open-browser --port=0
